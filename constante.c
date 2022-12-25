ed15814311800bb02833c511ba8f3169ebdf6b72
Andrew-cod <andrei.arapu@student.upt.ro>
2022-12-25 23:49:09 +0200

    am adaugat fisierul constate.c in are am dat exemple de constate si costate pointer constante pointer etc

---
+++ constante.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main(){
+    int i;//variabila simpla
+    const int a;//variabile de tip constanta
+    int *o;//pointer la o variabila de tip integer
+    const int *p;
+    int *const cpi=&i;//un pointer constant de tip int (pointerul nu isi poate schimba valoarea adica adresea la care arata)
+    
+    const int *const cpu=&a;//pointer constant la o constanta de tip integer
+
+
+
+
+
+    return 0;
+}
