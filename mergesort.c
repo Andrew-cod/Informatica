2f976f653f37afc5520a32b43731473baaf38ce3
Andrew-cod <andrei.arapu@student.upt.ro>
2023-01-02 11:28:57 +0200

    am scris singur merge sort

--- mergesort.c
+++ mergesort.c
@@ -1,62 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define print_tab(t,n) for(int i=0;i<n;i++)printf("%d ",t[i])
+void merge(int *t,int l ,int m ,int r){
+    int n_left=m-l+1;
+    int n_right= r-m;
 
-void merge(int arr[], int l, int m, int r)
-{
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
+    int t_left[n_left];
+    int t_right[n_right];
 
-    /* create temp arrays */
-    int L[n1], R[n2];
- 
-    /* Copy data to temp arrays L[] and R[] */
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
- 
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = l; // Initial index of merged subarray
+    for(int i=0;i<n_left;i++){
+        t_left[i]=t[i+l];
+    }
 
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
+    for(int i=0;i<n_right;i++){
+        t_right[i]=t[m+1+i];
+    }
+    int i=0,j=0;
+    for(int k=l;k<=r;k++){
+        if( t_left[i]<t_right[j] || (i<=m)&&(j>r) ){
+            t[k]=t_left[i];
             i++;
         }
         else {
-            arr[k] = R[j];
+            t[k]=t_right[j];
             j++;
         }
-        k++;
     }
- 
     
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
- 
-    
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+
+
+
+
+
+
 }
 
 void mergesort(int *t,int l,int r){
-    if(l<r){
-        int m= l+(r-l)/2;
-        mergesort(t,l,m);
-        mergesort(t,m+1,r);
+    int m=0;
+if(l<r){ 
+    m=l+(r-l)/2;
 
-        merge(t,l,m,r);
-    }
+    mergesort(t,l,m);
+    mergesort(t,m+1,r);
 
+    merge(t,l,m,r);
+}
 }
 
 int main(){
@@ -75,9 +63,7 @@ int main(){
     }
 printf("\n");
     mergesort(t,0,t_length-1);
-    for(int i=0;i<t_length;i++){
-        printf("%d ",t[i]);
-    }
+    print_tab(t,t_length);
 
 
 
