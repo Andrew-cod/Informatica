f8d881d5e7747773279eab7b4dcd3fd006abd449
Andrew-cod <andrei.arapu@student.upt.ro>
2023-01-02 11:49:56 +0200

    am adaugat commentarii la mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -2,21 +2,26 @@
 #include <stdlib.h>
 #define print_tab(t,n) for(int i=0;i<n;i++)printf("%d ",t[i])
 void merge(int *t,int l ,int m ,int r){
-    int n_left=m-l+1;
-    int n_right= r-m;
+    int n_left=m-l+1;//nr de elemente din partea stanga
+    int n_right= r-m;//ne de elemente din partea dreapta
 
-    int t_left[n_left];
-    int t_right[n_right];
+    int t_left[n_left];//declaram tabel pentru a copia elemetele din partea stanga
+    int t_right[n_right];//declaram tabel penteu a copia elementele din partea dreapta
 
+    //copiem
     for(int i=0;i<n_left;i++){
         t_left[i]=t[i+l];
     }
-
+    //copiem
     for(int i=0;i<n_right;i++){
         t_right[i]=t[m+1+i];
     }
+
+    //unim tabelele si le punem in tabelul initial
     int i=0,j=0;
     for(int k=l;k<=r;k++){
+        // daca intr-un tabel este mai mic elementul il punem din 1 tabel sau daca al 2 tabel sa terminat la fel punem in 1 tabel 
+        // altfel punem in tabelul 2
         if( t_left[i]<t_right[j] || (i<=m)&&(j>r) ){
             t[k]=t_left[i];
             i++;
@@ -26,45 +31,43 @@ void merge(int *t,int l ,int m ,int r){
             j++;
         }
     }
-    
-
-
-
-
-
 
 }
 
 void mergesort(int *t,int l,int r){
     int m=0;
+    //daca avem mai mult de 1 celula
 if(l<r){ 
     m=l+(r-l)/2;
+    //despartim tablelul in 2 apoi iarsi in 2 pana ajungem la 1 celula
+    mergesort(t,l,m);//sortam partea stanga
+    mergesort(t,m+1,r);//sortam partea dreapta
 
-    mergesort(t,l,m);
-    mergesort(t,m+1,r);
-
-    merge(t,l,m,r);
+    merge(t,l,m,r);//unim partile sortate
 }
 }
 
 int main(){
-
+    //declaram tabelul
     int t[100000];
     int t_length=0;
 
+    //citim tabelul
     scanf("%d",&t_length);
     for(int i=0;i<t_length;i++){
         scanf("%d",&t[i]);
     }
 
-
-    for(int i=0;i<t_length;i++){
-            printf("%d ",t[i]);
-    }
-printf("\n");
-    mergesort(t,0,t_length-1);
+    //afisam tabelul initial
     print_tab(t,t_length);
+    printf("\n");
 
+    //sortam tabelul
+    mergesort(t,0,t_length-1);
+    
+    //afisam tabelul sortat
+    printf("\n");
+    print_tab(t,t_length);
 
 
 
