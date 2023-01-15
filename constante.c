ead8f524b6010de707b578c37c24c8aa1a4a91bb
Andrew-cod <andrei.arapu@student.upt.ro>
2023-01-15 23:11:24 +0200

    am adaugat constate.c si am copiat grila.c

--- constante.c
+++ constante.c
@@ -4,11 +4,21 @@ int main(){
     
     int i;//variabila simpla
     const int a;//variabile de tip constanta
-    int *o;//pointer la o variabila de tip integer
-    const int *p;
-    int *const cpi=&i;//un pointer constant de tip int (pointerul nu isi poate schimba valoarea adica adresea la care arata)
-    
+    int *o=&i;//pointer la o variabila de tip integer
+    const int *p;//Adresa care este păstrată în pointer poate fi schimbată, însă valoarea de la adresa respectivă nu poate fi modificată
+
+
+
+
+
+
+    int *const cpi=&i;//putem schimba valoarea variabilei a cărei adresă o păstrează pointerul, însă acesta nu va putea indica decât spre acea zonă de memorie pe tot parcursul programului.
+
+    //albele
     const int *const cpu=&a;//pointer constant la o constanta de tip integer
+
+
+
 
 
 
