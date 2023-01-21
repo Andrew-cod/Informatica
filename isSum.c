67b694fd685b90a23779a62e8eb294cd1c24d07a
Andrew-cod <andrei.arapu@student.upt.ro>
2023-01-21 23:58:07 +0200

    am rezolvat problema se da un nr si un array de numere determinati cea mai scurta metota de a forma din numerele din array numarul nr dat la inceput

--- isSum.c
+++ isSum.c
@@ -1,33 +1,116 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-int issum(int target, int *t, int n,int *rez)
-{   
-    if(target<0)return 0;
-    if (target == 0)return 1;
-    if(rez[target]!=0)return rez[target];
-    for (int i = 0; i < n; i++)
+
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    /* create temp arrays */
+    int L[n1], R[n2];
+
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2)
     {
-        rez[target]=issum(target - t[i], t, n,rez);
-        if(rez[target]==1)return 1;
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
     }
-    return 0;
+
+    /* Copy the remaining elements of L[], if there
+    are any */
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    /* Copy the remaining elements of R[], if there
+    are any */
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+/* l is for left index and r is right index of the
+sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+int issum(int target, int *t, int n, int *rez)
+{
+    if (target < 0)
+        return 0;
+    if (target == 0)
+        return 1;
+    if (rez[target] == 1)
+        return 1;
+    for (int i = n-1; i >=0; i--)
+    {
+        rez[target] = issum(target - t[i], t, n, rez);
+        if (rez[target] == 1)
+        {
+            printf("%d ", t[i]);
+            return 1;
+        }
+    }
 }
 
 int main()
-{   
+{
 
     int target, n;
     scanf("%d%d", &target, &n);
-    int t[n],*rez;
-    rez=(int*)calloc(target,sizeof(int));
+    int t[n], *rez;
+    rez = (int *)calloc(target, sizeof(int));
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &t[i]);
     }
 
-    int r = issum(target, t, n,rez);
-    printf("\n%d", r);
+    mergeSort(t,0,n-1);
+
+    int r = issum(target, t, n, rez);
+    if (r == 1)
+        printf("\nPossibil");
+    else
+        printf("\nImposibil");
 
     return 0;
 }
